c297cce233e42706aa76b65090344c2d5703bd08
javilolez <javilopezleza90@gmail.com>
2022-07-08 13:25:42 +0200

    Añadida pruebas nuevas en el componente AddCategory

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -11,7 +11,6 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     const onSubmit = (event) => {
-
         event.preventDefault();
         if (inputValue.trim().length <= 1) return;
 
@@ -24,7 +23,7 @@ export const AddCategory = ({ onNewCategory }) => {
     }
 
     return (
-        <form onSubmit={(event) => onSubmit(event)}>
+        <form aria-label="form" onSubmit={(event) => onSubmit(event)}>
             <input
                 type="text"
                 placeholder="Buscar gifs"
--- src/tests/components/AddCategory.test.jsx
+++ src/tests/components/AddCategory.test.jsx
@@ -7,17 +7,53 @@ describe('Tests in <AddCategory />', () => {
     test('should change the value in the textbox', () => {
 
 
-        render(<AddCategory onNewCategory={() => {}} />);
+        render(<AddCategory onNewCategory={() => { }} />);
         const input = screen.getByRole('textbox');
-        
-        fireEvent.input( input, {target:{value:'Saitama'}} );
+
+        fireEvent.input(input, { target: { value: 'Saitama' } });
 
         expect(input.value).toBe('Saitama');
         screen.debug();
 
+    });
+
+    test('should call inNewCategory if the input have a value ', () => {
+
+        const inputValue = 'Saitama';
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input(input, { target: { value: inputValue } });
+        fireEvent.submit(form);
+
+        expect(input.value).toBe('');
+
+        expect(onNewCategory).toHaveBeenCalled(); //Comprueba si la funcion ha sido llamada
+        expect(onNewCategory).toHaveBeenCalledTimes(1); //Comprueba si la funcion ha sido llamada X cantidad de veces
+        expect(onNewCategory).toHaveBeenCalledWith(inputValue); //Comprueba si la funcion ha sido llamada con un valor
+
 
     });
 
+    test("shouldn't call onNewCategory if the input it's void", () => { 
+
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={onNewCategory} />);
+
+        const form = screen.getByRole('form');
+
+        fireEvent.submit(form);
+
+        expect(onNewCategory).toHaveBeenCalledTimes(0); //Comprueba si la funcion ha sido llamada X cantidad de veces
+        expect(onNewCategory).not.toHaveBeenCalled(); //Comprueba si no se ha llamado la funcion
+
+
+
+     });
 
 });
 
